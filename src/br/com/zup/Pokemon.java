2e8ccc6278b8aff991941e0031c757bd82b76df8
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-11-20 12:38:40 -0300

    Modelar Classe Pokemon

--- src/br/com/zup/Pokemon.java
+++ src/br/com/zup/Pokemon.java
@@ -1,5 +1,118 @@
 package br.com.zup;
 
+import java.util.List;
+
 public class Pokemon {
 
+    private int pokedex;
+    private String nome;
+    private Tipo tipoUm;
+    private Tipo tipoDois;
+    private int hp;
+    private int defense;
+    private int spDef;
+    private int attack;
+    private int spAtk;
+    private int speed;
+    private List<Move> moveList;
+    private List<Ability>abilityList;
+
+    public Pokemon() {
+    }
+
+    public int getPokedex() {
+        return pokedex;
+    }
+
+    public void setPokedex(int pokedex) {
+        this.pokedex = pokedex;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Tipo getTipoUm() {
+        return tipoUm;
+    }
+
+    public void setTipoUm(Tipo tipoUm) {
+        this.tipoUm = tipoUm;
+    }
+
+    public Tipo getTipoDois() {
+        return tipoDois;
+    }
+
+    public void setTipoDois(Tipo tipoDois) {
+        this.tipoDois = tipoDois;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public void setHp(int hp) {
+        this.hp = hp;
+    }
+
+    public int getDefense() {
+        return defense;
+    }
+
+    public void setDefense(int defense) {
+        this.defense = defense;
+    }
+
+    public int getSpDef() {
+        return spDef;
+    }
+
+    public void setSpDef(int spDef) {
+        this.spDef = spDef;
+    }
+
+    public int getAttack() {
+        return attack;
+    }
+
+    public void setAttack(int attack) {
+        this.attack = attack;
+    }
+
+    public int getSpAtk() {
+        return spAtk;
+    }
+
+    public void setSpAtk(int spAtk) {
+        this.spAtk = spAtk;
+    }
+
+    public int getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(int speed) {
+        this.speed = speed;
+    }
+
+    public List<Move> getMoveList() {
+        return moveList;
+    }
+
+    public void setMoveList(List<Move> moveList) {
+        this.moveList = moveList;
+    }
+
+    public List<Ability> getAbilityList() {
+        return abilityList;
+    }
+
+    public void setAbilityList(List<Ability> abilityList) {
+        this.abilityList = abilityList;
+    }
 }

63540bf3dd622d20e9d48b6e0b16994c3b356ee1
Yan Rodrigues Santos <yyan1207@gmail.com>
2021-11-20 12:42:55 -0300

    Modelar Classe Ability

--- src/br/com/zup/Ability.java
+++ src/br/com/zup/Ability.java
@@ -1,4 +1,16 @@
 package br.com.zup;
 
 public class Ability {
+    private String nome;
+
+    public Ability() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
 }

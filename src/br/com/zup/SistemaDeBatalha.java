c6de0951cbcf52c69fba4387325fa7e34a4783fb
yan-rds <yyan1207@gmail.com>
2021-11-24 13:32:34 -0300

    Criar Scanner no SistemaDeBatalha

--- src/br/com/zup/SistemaDeBatalha.java
+++ src/br/com/zup/SistemaDeBatalha.java
@@ -1,4 +1,12 @@
 package br.com.zup;
 
+import java.util.Scanner;
+
 public class SistemaDeBatalha {
+
+    private static Scanner leitor (String mensagem){
+        System.out.println(mensagem);
+        return new Scanner(System.in);
+    }
+
 }

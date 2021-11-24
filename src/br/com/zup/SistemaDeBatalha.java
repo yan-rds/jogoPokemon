0c58d1d2b5c59da69644afabfa767efeb9f1ce37
yan-rds <yyan1207@gmail.com>
2021-11-24 13:44:31 -0300

    Criar método escolherAtaque

--- src/br/com/zup/SistemaDeBatalha.java
+++ src/br/com/zup/SistemaDeBatalha.java
@@ -16,7 +16,7 @@ public class SistemaDeBatalha {
         int escolha = leitor("").nextInt();
 
         if (escolha == 1){
-            // Método de selecionar os Ataques
+            escolherAtaque(new Pokemon()/*teste*/);
         }
 
         else if (escolha == 2){
@@ -27,6 +27,27 @@ public class SistemaDeBatalha {
             // Método de trocar de pokemons
         }
 
+    }
+
+    public static void escolherAtaque (Pokemon pokemon){
+        System.out.println("1-" +pokemon.getMoves.get(0));
+        System.out.println("2-" +pokemon.getMoves.get(1));
+        System.out.println("3-" +pokemon.getMoves.get(2));
+        System.out.println("4-" +pokemon.getMoves.get(3));
+        int escolha = leitor("").nextInt();
+
+        if (escolha == 1){
+            //Método que calcula dano utilizando o move 1 como parametro
+        }
+        else if (escolha == 2){
+            //Método que calcula dano utilizando o move 2 como parametro
+        }
+        else if (escolha == 3){
+            //Método que calcula dano utilizando o move 3 como parametro
+        }
+        else if (escolha == 4){
+            //Método que calcula dano utilizando o move 4 como parametro
+        }
     }
 
 

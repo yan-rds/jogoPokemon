11b86d3cc261e0ce1e5ae870791cf6eab792ef21
yan-rds <yyan1207@gmail.com>
2021-11-24 13:34:15 -0300

    Criar Método turnoDoJogador

--- src/br/com/zup/SistemaDeBatalha.java
+++ src/br/com/zup/SistemaDeBatalha.java
@@ -9,4 +9,25 @@ public class SistemaDeBatalha {
         return new Scanner(System.in);
     }
 
+    public static void turnoDoJogador (){
+        System.out.println("1 - Atacar");
+        System.out.println("2 - Utilizar Itens");
+        System.out.println("3 - Trocar de pokemon");
+        int escolha = leitor("").nextInt();
+
+        if (escolha == 1){
+            // Método de selecionar os Ataques
+        }
+
+        else if (escolha == 2){
+            // Método de utilizar itens
+        }
+
+        else if (escolha == 3){
+            // Método de trocar de pokemons
+        }
+
+    }
+
+
 }
